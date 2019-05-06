5c7cc30d7f862292e9a17180be15ba7b279b352a
ashevin <avi@kik.com>
2019-05-06 09:39:32 +0300

    Minor refactoring.

--- Sources/nova/Commands.swift
+++ Sources/nova/Commands.swift
@@ -74,3 +74,92 @@ func data(account: StellarAccount, key: String, val: Data?, fee: UInt32? = nil) 
             return ErrorMessage(message: "Received error while setting data: \($0)")
         })
 }
+
+func dump() {
+    do {
+        let data = try Data(contentsOf: URL(fileURLWithPath: cnf.file))
+        let uncompressed = cnf.file.hasSuffix(".gz")
+            ? try data.gunzipped()
+            : data
+
+        let jsonEnc = JSONEncoder()
+        jsonEnc.outputFormatting = .prettyPrinted
+
+        let filename = cnf.file.split(separator: "/").last!
+        if filename.starts(with: "ledger-") {
+            let results: [LedgerHeaderHistoryEntry] = try parse(data: uncompressed)
+
+            let json = try jsonEnc.encode(results)
+            print(String(bytes: json, encoding: .utf8)!)
+        }
+        else if filename.starts(with: "transactions-") {
+            let results: [TransactionHistoryEntry] = try parse(data: uncompressed)
+
+            let json = try jsonEnc.encode(results)
+            print(String(bytes: json, encoding: .utf8)!)
+        }
+        else if filename.starts(with: "results-") {
+            let results: [TransactionHistoryResultEntry] = try parse(data: uncompressed)
+
+            let json = try jsonEnc.encode(results)
+            print(String(bytes: json, encoding: .utf8)!)
+        }
+        else if filename.starts(with: "bucket-") {
+            let results: [BucketEntry] = try parse(data: uncompressed)
+
+            let json = try jsonEnc.encode(results)
+            print(String(bytes: json, encoding: .utf8)!)
+        }
+        else {
+            print("Unknown archive type.")
+        }
+    }
+    catch {
+        print(error)
+    }
+}
+
+func list(whitelist: StellarAccount) {
+    var waiting = true
+
+    whitelist.details(node: node)
+        .then({ details in
+            let overrides = details.data.filter { $0.0.starts(with: "priority_count_") }
+            let keys = details.data.filter { $0.0.utf8.count == 56 }
+
+            if let reserve = details.data.filter({ $0.0 == "reserve" }).first {
+                var r = Int32()
+                read(4, from: Data(base64Encoded: reserve.1)!, into: &r)
+
+                print("Reserve: \(r.bigEndian)\n")
+            }
+
+            print("Priority overrides")
+            print("------------------")
+            for p in overrides.keys.sorted() {
+                let v = overrides[p]!
+                print("  \(p.suffix(2)): \(Array(Data(base64Encoded: v)!))")
+            }
+
+            print("\nKeys                                                        Priority")
+            print("--------------------------------------------------------------------")
+            for k in keys {
+                var p = ""
+
+                let d = Data(base64Encoded: k.1)!
+                if d.count == 8 {
+                    let pd = d.suffix(4)
+                    var pi = Int32()
+                    read(4, from: pd, into: &pi)
+
+                    p = String(describing: pi.bigEndian)
+                }
+
+                print("\(k.0)\t\(p)")
+            }
+        })
+        .error { print($0) }
+        .finally { waiting = false }
+
+    while waiting {}
+}
